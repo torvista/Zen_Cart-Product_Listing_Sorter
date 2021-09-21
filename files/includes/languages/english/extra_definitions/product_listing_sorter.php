a1aa94e6d3469ca1e0ec66f8d27a2c4d8fb3a2cc
Steve <4391026+torvista@users.noreply.github.com>
2021-09-21 09:01:39 +0200

    backup

--- files/includes/languages/english/extra_definitions/product_listing_sorter.php
+++ files/includes/languages/english/extra_definitions/product_listing_sorter.php
@@ -3,7 +3,7 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  */
 define('TEXT_PRODUCT_LISTING_SORTER_CHOOSE', 'Choose...'); // first item in drop-down when no sorting options selected
-define('TEXT_PRODUCT_LISTING_SORTER_RESET', TEXT_PRODUCTS_LISTING_ALPHA_SORTER_NAMES_RESET);   // first item in drop-down when a sorting option is already in use
+define('TEXT_PRODUCT_LISTING_SORTER_RESET', '-- Reset --');   // first item in drop-down when a sorting option is already in use
 define('TEXT_IMAGE', 'Image');
 define('TEXT_MODEL', 'Model');
 define('TEXT_NAME', 'Name');
--- sql/product_listing_sorter_install.sql
+++ sql/product_listing_sorter_install.sql
@@ -1,4 +1,4 @@
-#add admin switch into the Admin->Product Lisintg page
+#add admin switch into the Admin->Product Listing page
 INSERT INTO configuration 
 (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) VALUES 
-('Include Product Listing Sorter Dropdown (mod)', 'PRODUCT_LISTING_SORTER', 'true', 'Do you want to include the Product Listing Sorter dropdown on the Product Listing page?', '8', '51', 'zen_cfg_select_option(array(\'true\', \'false\'), ', now());
+('Include Product Listing Sorter Dropdown (plugin)', 'PRODUCT_LISTING_SORTER', 'true', 'Do you want to show the Product Listing Sorter dropdown on the Product Listing page?', '8', '51', 'zen_cfg_select_option(array(\'true\', \'false\'), ', now());
